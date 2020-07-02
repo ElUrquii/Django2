08adfc9db11697f4472323a6d3521f3a856701a4
ElUrquii <urquizamaxi099@gmail.com>
2020-07-02 17:39:59 -0300

    Consigna 6

--- config/Comercio/admin.py
+++ config/Comercio/admin.py
@@ -4,8 +4,8 @@ from .models import *
 # Register your models here.
 
 class ClienteAdmin(admin.ModelAdmin):
-    list_display = ['rut', 'nombre', 'telefono']
-    list_display_links = ['rut', 'nombre', 'telefono']
+    list_display = ('rut', 'nombre', 'telefono')
+    list_display_links = ('rut', 'nombre', 'telefono')
 
 class ProductoAdmin(admin.ModelAdmin):
     readonly_fields = ('id',)
@@ -19,16 +19,29 @@ class ProductoAdmin(admin.ModelAdmin):
     )
 
 class ProveedorAdmin(admin.ModelAdmin):
-    list_display = ['nombre', 'web']
-    list_filter = ['nombre', 'rut']
+    list_display = ('nombre', 'web')
+    list_filter = ('nombre', 'rut')
 
+class VentaAdmin(admin.ModelAdmin):
+    list_display = ('cliente', 'fecha', 'isDescuento')
+    list_display_links = ('cliente', 'fecha')
+
+    actions = ['aplicarDescuento', 'quitarDescuento']
+
+    def aplicarDescuento(self, request, queryset):
+        queryset.update(descuento = True)
+    aplicarDescuento.short_description = 'Aplicar Descuento'
+
+    def quitarDescuento(self, request, queryset):
+        queryset.update(descuento = False)
+    quitarDescuento.short_description = 'Quitar Descuento'
 
 admin.site.register(Categoria)
 admin.site.register(Ciudad)
 admin.site.register(Comuna)
 admin.site.register(Direccion)
 admin.site.register(Cliente, ClienteAdmin)
-admin.site.register(Venta)
+admin.site.register(Venta, VentaAdmin)
 admin.site.register(Proveedor, ProveedorAdmin)
 admin.site.register(Producto, ProductoAdmin)
 admin.site.register(Detalle)
