fad9ece93963de161985b432a12e13a25ff394a7
ElUrquii <urquizamaxi099@gmail.com>
2020-07-02 17:00:35 -0300

    Consigna 3

--- config/Comercio/admin.py
+++ config/Comercio/admin.py
@@ -3,11 +3,16 @@ from .models import *
 
 # Register your models here.
 
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ['rut', 'nombre', 'telefono']
+    list_display_links = ['rut', 'nombre', 'telefono']
+
+
 admin.site.register(Categoria)
 admin.site.register(Ciudad)
 admin.site.register(Comuna)
 admin.site.register(Direccion)
-admin.site.register(Cliente)
+admin.site.register(Cliente, ClienteAdmin)
 admin.site.register(Venta)
 admin.site.register(Proveedor)
 admin.site.register(Producto)
