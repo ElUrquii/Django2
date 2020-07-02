cc6da8ae4ab4ca78a14599c97284df79e6551446
ElUrquii <urquizamaxi099@gmail.com>
2020-07-02 16:51:29 -0300

    Cambios al Models.py

--- config/Comercio/admin.py
+++ config/Comercio/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(Categoria)
+admin.site.register(Ciudad)
+admin.site.register(Comuna)
+admin.site.register(Direccion)
+admin.site.register(Cliente)
+admin.site.register(Venta)
+admin.site.register(Proveedor)
+admin.site.register(Producto)
+admin.site.register(Detalle)
--- config/Comercio/models.py
+++ config/Comercio/models.py
@@ -1,3 +1,85 @@
 from django.db import models
 
 # Create your models here.
+
+class Categoria(models.Model):
+    id = models.AutoField(primary_key = True)
+    nombre = models.CharField(max_length = 30)
+    descripcion = models.CharField(max_length = 144)
+
+    def __str__(self):
+        return '{}'.format(self.nombre)
+
+class Comuna(models.Model):
+    nombre = models.CharField(max_length = 30)
+    ciudad = models.ForeignKey('Ciudad', on_delete = models.CASCADE, null = False)
+
+    def __str__(self):
+        return str(self.nombre)
+
+class Ciudad(models.Model):
+    nombre = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return str(self.nombre)
+
+class Direccion(models.Model):
+    numero = models.CharField(max_length = 5)
+    calle = models.CharField(max_length = 50)
+    comuna = models.ForeignKey('Comuna', on_delete = models.CASCADE)
+
+    def __str__(self):
+        return str("{}, {}".format(self.calle, self.numero))
+
+class Proveedor(models.Model):
+    rut = models.AutoField(primary_key = True)
+    nombre = models.CharField(max_length = 20)
+    web = models.CharField(max_length = 50)
+    telefono = models.IntegerField()
+    direccion = models.ForeignKey('Direccion', on_delete = models.CASCADE)
+
+    def __str__(self):
+        return '{}'.format(self.nombre)
+
+class Producto(models.Model):
+    id = models.AutoField(primary_key = True)
+    nombre = models.CharField(max_length = 20)
+    precio = models.FloatField()
+    stock = models.IntegerField()
+    proveedor = models.ForeignKey('Proveedor', on_delete = models.CASCADE)
+    categoria = models.ForeignKey('Categoria', on_delete = models.CASCADE)
+
+    def __str__(self):
+        return '{}'.format(self.nombre)
+
+class Cliente(models.Model):
+    rut = models.AutoField(primary_key = True)
+    nombre = models.CharField(max_length = 20)
+    telefono = models.IntegerField()
+    direccion = models.ForeignKey('Direccion', on_delete = models.CASCADE)
+
+    def __str__(self):
+        return '{}'.format(self.nombre)
+
+class Venta(models.Model):
+    id = models.AutoField(primary_key = True)
+    fecha = models.DateField()
+    monto_final = models.FloatField()
+    descuento = models.BooleanField()
+    cliente = models.ForeignKey('Cliente', on_delete = models.CASCADE)
+
+    def isDescuento(self):
+        return self.descuento
+
+    isDescuento.boolean = True
+    isDescuento.short_description = 'Descuento'
+
+    def __str__(self):
+        return str('Venta al Cliente: {}'.format(self.cliente.nombre))
+
+class Detalle(models.Model):
+    producto = models.ForeignKey('Producto', on_delete=models.CASCADE, null=False)
+    cantidad = models.SmallIntegerField()
+    venta = models.ForeignKey('Venta',on_delete=models.CASCADE,null=False)
+    def __str__(self):
+        return str("({}) {} a {}".format(self.cantidad,self.producto.nombre,self.venta.cliente.nombre))
